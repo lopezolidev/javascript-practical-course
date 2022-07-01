730dc826dda204e5fc92fddaaa392f639ec11bb3
rockwatch <rocksistor@gmail.com>
2022-07-01 14:17:05 -0400

    added median, mode and geometric average

--- statistics/median.js
+++ statistics/median.js
@@ -6,7 +6,7 @@ function calculateAverage(list) {
     )
 
     const average = sum / list.length;
-    return average;
+    console.log(average);
 }
 
 const list = [
@@ -35,5 +35,56 @@ if (isEven(list.length)) {
 }  
 
 // challenge:   1° transform this into a function
+
+function calculateMedian(anyArray) {
+    const halfArray = parseInt((anyArray.length / 2));
+
+    if (anyArray.length % 2 === 0) {
+        const value1 = anyArray[halfArray - 1 ];
+        const value2 = anyArray[halfArray];
+        calculateAverage([value1, value2])
+    } else {
+        console.log(anyArray[halfArray]);
+    }  
+    
+}
+
+console.log(anyArray);
+
 //              2° make the parameters any list
+
+
 //              3° sort the list in the correct form
+
+const anyArray = [2, 34, 5, 24, 31, 23, 18, 49, 60, 9, 38, 6];
+
+function calculateAverage(list) {
+    const sum = list.reduce(
+        function (accumulatedValue = 0, newValue) {
+            return accumulatedValue + newValue;
+        }
+    )
+
+    const average = sum / list.length;
+    console.log(average);
+}
+
+const orderedArray = anyArray.sort( function(a, b) {
+    return a - b;
+});
+
+function calculateMedian(orderedArray) {
+
+    const halfArray = parseInt((orderedArray.length / 2));
+
+    if (orderedArray.length % 2 === 0) {
+        const value1 = orderedArray[halfArray - 1 ];
+        const value2 = orderedArray[halfArray];
+        calculateAverage([value1, value2])
+    } else {
+        console.log(orderedArray[halfArray]);
+    }  
+    
+}
+
+calculateMedian(orderedArray);
---
+++ statistics/mode.js
@@ -0,0 +1,107 @@
+// teachers method to calculate the mode
+
+const list = [
+    1,
+    1,
+    2,
+    3,
+    4,
+    2,
+    2,
+    1,
+    3,
+    4,
+    2,
+    2,
+    2,
+    1,
+];
+
+const listObject = {};
+
+list.map(
+    function (element) {
+        if (listObject[element]) {
+            listObject[element] += 1;
+        }
+        else { 
+            listObject[element] = 1;
+        }
+    }
+);
+
+const listArray = Object.entries(listObject).sort(
+    function (elementA, elementB) {
+        return elementA[1] - elementB[1];
+    }
+);
+
+const mode = listArray[listArray.length - 1];
+
+// challenge: transform this code into a function
+// let x = 0;
+// const newList = [];
+// while ( x <= 12) {
+//     newList.push(Number(prompt("insert new number to array")))
+//     x += 1;
+//     console.log(newList);
+// }
+
+const newList = [];
+function mode(newList) {
+    let x = 0;
+    let y = Number(prompt("insert length of array"));
+    while ( x <= (y - 1) ) {
+        newList.push(Number(prompt("insert new number to array")))
+        x += 1;
+        console.log(newList);
+    }
+
+    const listObject = {};
+
+    newList.map(
+        function (element) {
+            if (listObject[element]) {
+                listObject[element] += 1;
+            }
+            else { 
+                listObject[element] = 1;
+            }
+        }
+    );
+    const listArray = Object.entries(listObject).sort(
+        function (elementA, elementB) {
+            return elementA[1] - elementB[1];
+        }
+    );
+
+    const mode = listArray[listArray.length - 1];
+    return mode;
+
+}
+
+mode(newList);
+
+//challenge: find geometric average
+
+const geoValues = [];
+function geometricAverage(geoValues) {
+    let y = Number(prompt("insert length of array"));
+    let x = 0;
+    while (x <= (y -1) ) {
+        geoValues.push(Number(prompt("insert number")));
+        x += 1;
+        console.log(geoValues);
+    }
+
+    const valuesTotal = geoValues.reduce( function(previous = geoValues[0], current) {
+        const sequence = previous * current;
+        return sequence
+    });
+
+    const geometricV = Math.pow(valuesTotal, 1 / y);
+
+    return Math.round(geometricV);
+
+}
+geometricAverage(geoValues);
