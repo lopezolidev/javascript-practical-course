52165e2fca72ec1f033f65ae0ec8912e18aea353
rockwatch <rocksistor@gmail.com>
2022-07-14 13:07:45 -0400

    converted npv calculus into a function and started drafting html file to interact with npv code

--- final_challenge/feasibility.js
+++ final_challenge/feasibility.js
@@ -24,3 +24,41 @@ const npv = total - investment;
 
 console.log(npv);
 
+// now with a function
+
+const anyArray = [];
+
+function netPresentValue (values) {
+    const investment = Number(document.getElementById("inputInvestment"));
+    const periods = Number(document.getElementById("inputPeriod"));
+    const rate = Number(document.getElementById("inputRate"));
+
+    for (var i = 0; i < periods; i++) {
+        const newValues = values.push(Number(prompt("new value")));
+        console.log(values);
+    }
+
+    const newValues = [];
+
+    for (let j = 1; j < values.length + 1; j++) {
+        newValues.push(parseInt(values[j-1] / Math.pow(1 + rate, j)));
+        console.log(newValues);
+    }
+
+    const total = newValues.reduce( function (initialValue, finalValue) {
+        return initialValue + finalValue;
+    }, 0)
+
+    const npv = parseInt(total - investment);
+
+    console.log(npv);
+
+    if (npv > 0) {
+        console.log("The npv value is" + npv + ", with an investment of " + investment + ", an interest rate of " + rate + " and " + periods + " periods, is feasible")
+    }
+    else {
+        console.log("The npv value is " + npv + ", the investment wasn't feasible.")
+    }
+}
+
+netPresentValue(anyArray);
